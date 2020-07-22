876de7fb82b491416ba0f5958f9fb3939ccac0d2
chaojie.jiang <chaojie.jiang@vbooster.cn>
2020-07-22 10:56:16 +0800

    功能补充

---
+++ outboundmanage/src/main/java/com/smartindustry/outbound/constant/MaStatusEnum.java
@@ -0,0 +1,10 @@
+package com.smartindustry.outbound.constant;
+
+/**
+ * @author: jiangzhaojie
+ * @date: Created in 9:15 2020/7/22
+ * @version: 1.0.0
+ * @description:
+ */
+public enum MaStatusEnum {
+}
