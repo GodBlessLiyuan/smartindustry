c4cb8d1fa5c416cf90de2725140e6018fe2a0608
xiahui <hui.xia@rpahelper.com>
2020-07-24 20:05:26 +0800

    初始化数据脚本。

---
+++ initdata.sql
@@ -0,0 +1,5 @@
+insert into si_material(material_no, material_name, material_type, material_model, material_desc, test_type, dr) values('5101000496', '原料物料001',1, 'SH0001', '测试物料，原料类型', 1, 1);
+insert into si_material(material_no, material_name, material_type, material_model, material_desc, test_type, dr) values('5101000497', '半成品物料002',2, 'SH0002', '测试物料，半成品类型', 2, 1);
+
+insert into si_location(location_no, user_id, dr ) values('8-10', 1, 1);
+insert into si_location(location_no, user_id, dr ) values('8-11', 1, 1);
