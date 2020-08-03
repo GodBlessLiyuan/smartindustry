dec2b265e4a74c0e55461acc2e58af19b1fe0399
chaojie.jiang <chaojie.jiang@vbooster.cn>
2020-08-03 10:06:15 +0800

    登入

--- authoritymanage/src/main/java/com/smartindustry/authority/service/ILoginService.java
+++ authoritymanage/src/main/java/com/smartindustry/authority/service/ILoginService.java
@@ -1,22 +1,23 @@
-//package com.smartindustry.authority.service;
-//
-//import com.smartindustry.authority.dto.LoginDTO;
-//
-//import javax.servlet.http.HttpServletResponse;
-//import javax.servlet.http.HttpSession;
-//
-///**
-// * @author: jiangzhaojie
-// * @date: Created in 9:22 2020/8/3
-// * @version: 1.0.0
-// * @description:
-// */
-//public interface ILoginService {
-//    /**
-//     * 登入
-//     * @param dto
-//     * @return
-//     */
-////    String login(HttpSession session,
-////                 HttpServletResponse response,LoginDTO dto);
-//}
+package com.smartindustry.authority.service;
+
+import com.smartindustry.authority.dto.LoginDTO;
+import com.smartindustry.common.vo.ResultVO;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * @author: jiangzhaojie
+ * @date: Created in 9:22 2020/8/3
+ * @version: 1.0.0
+ * @description:
+ */
+public interface ILoginService {
+    /**
+     * 登入
+     * @param dto
+     * @return
+     */
+    ResultVO login(HttpSession session,
+                   HttpServletResponse response, LoginDTO dto);
+}
--- authoritymanage/src/main/java/com/smartindustry/authority/service/impl/LoginServiceImpl.java
+++ authoritymanage/src/main/java/com/smartindustry/authority/service/impl/LoginServiceImpl.java
@@ -1,62 +1,63 @@
-//package com.smartindustry.authority.service.impl;
-//
-//import com.google.code.kaptcha.Constants;
-//import com.smartindustry.authority.dto.LoginDTO;
-//import com.smartindustry.authority.service.ILoginService;
-//import com.smartindustry.common.pojo.am.UserPO;
-//import com.smartindustry.common.vo.ResultVO;
-//import org.springframework.stereotype.Service;
-//
-//import javax.servlet.http.HttpServletResponse;
-//import javax.servlet.http.HttpSession;
-//
-///**
-// * @author: jiangzhaojie
-// * @date: Created in 9:23 2020/8/3
-// * @version: 1.0.0
-// * @description:
-// */
-//@Service
-//public class LoginServiceImpl implements ILoginService {
-//
-////    @Override
-////    public ResultVO login(HttpSession session,
-////                        HttpServletResponse response,LoginDTO dto){
-////
-////
-////        // 首先根据username查询个人信息
-////        UserPO userPo = userMapper.queryByName(dto.getUsername());
-////
-////        if (null!=userPo) {
-////            // 查询到的编码是加密过的
-////            String realPassword = userPo.getPassword();
-//////            try {
-//////                password = Md5Util.encodeByMd5(SALT + password);
-//////            } catch (Exception e) {
-//////                e.printStackTrace();
-//////            }
-////            if (!password.equals(realPassword)) {
-////                //密码错误
-////                return new ResultVO(2101);
-////            }
-////        } else { //用户名错误
-////            return new ResultVO(2100);
-////        }
-////
-////        // 得到验证码
-////        String code = (String) session.getAttribute(Constants.KAPTCHA_SESSION_KEY);
-////        if (null == code) {
-////            // 验证码过期
-////            return new ResultVO(2102);
-////        }
-////        if (!code.equalsIgnoreCase(checkcode)) {
-////            //验证码错误
-////            return new ResultVO(2103);
-////        }
-////        // 生成token
-////
-////
-////        return ResultVO.ok().setData(11);
-////    }
-////
-//}
+package com.smartindustry.authority.service.impl;
+
+import com.google.code.kaptcha.Constants;
+import com.smartindustry.authority.dto.LoginDTO;
+import com.smartindustry.authority.service.ILoginService;
+import com.smartindustry.common.mapper.am.UserMapper;
+import com.smartindustry.common.pojo.am.UserPO;
+import com.smartindustry.common.vo.ResultVO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * @author: jiangzhaojie
+ * @date: Created in 9:23 2020/8/3
+ * @version: 1.0.0
+ * @description:
+ */
+@Service
+public class LoginServiceImpl implements ILoginService {
+    @Autowired
+    UserMapper userMapper;
+
+    @Override
+    public ResultVO login(HttpSession session,
+                        HttpServletResponse response,LoginDTO dto){
+        // 首先根据username查询个人信息
+        UserPO userPo = userMapper.queryByName(dto.getUsername());
+
+        if (null!=userPo) {
+            // 查询到的编码是加密过的
+            String realPassword = userPo.getPassword();
+//            try {
+//                password = Md5Util.encodeByMd5(SALT + password);
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//            }
+            if (!dto.getPassword().equals(realPassword)) {
+                //密码错误
+                return new ResultVO(2101);
+            }
+        } else { //用户名错误
+            return new ResultVO(2100);
+        }
+
+        // 得到验证码
+        String code = (String) session.getAttribute(Constants.KAPTCHA_SESSION_KEY);
+        if (null == code) {
+            // 验证码过期
+            return new ResultVO(2102);
+        }
+        if (!code.equalsIgnoreCase(dto.getCode())) {
+            //验证码错误
+            return new ResultVO(2103);
+        }
+        // 生成token
+
+        return ResultVO.ok();
+    }
+
+}
