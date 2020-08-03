97410fcc65b19e0b98f914585d996f2875b925ef
chaojie.jiang <chaojie.jiang@vbooster.cn>
2020-08-03 09:43:48 +0800

    d

---
+++ authoritymanage/src/main/java/com/smartindustry/authority/dto/LoginDTO.java
@@ -0,0 +1,19 @@
+package com.smartindustry.authority.dto;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * @author: jiangzhaojie
+ * @date: Created in 9:16 2020/8/3
+ * @version: 1.0.0
+ * @description:
+ */
+@Data
+public class LoginDTO implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private String username;
+    private String password;
+    private String code;
+}
---
+++ authoritymanage/src/main/java/com/smartindustry/authority/service/ILoginService.java
@@ -0,0 +1,22 @@
+//package com.smartindustry.authority.service;
+//
+//import com.smartindustry.authority.dto.LoginDTO;
+//
+//import javax.servlet.http.HttpServletResponse;
+//import javax.servlet.http.HttpSession;
+//
+///**
+// * @author: jiangzhaojie
+// * @date: Created in 9:22 2020/8/3
+// * @version: 1.0.0
+// * @description:
+// */
+//public interface ILoginService {
+//    /**
+//     * 登入
+//     * @param dto
+//     * @return
+//     */
+////    String login(HttpSession session,
+////                 HttpServletResponse response,LoginDTO dto);
+//}
