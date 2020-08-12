75c9abf2e610c6af2f01135d5c76a81d20904950
xiahui <hui.xia@rpahelper.com>
2020-08-12 16:27:07 +0800

    库存信息添加权限。

--- inventorymanage/src/main/java/com/smartindustry/inventory/InventoryApplication.java
+++ inventorymanage/src/main/java/com/smartindustry/inventory/InventoryApplication.java
@@ -3,6 +3,7 @@ package com.smartindustry.inventory;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @author: xiahui
@@ -10,7 +11,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @description: 库存信息
  * @version: 1.0
  */
-@MapperScan("com.smartindustry.common.mapper")
+@MapperScan({"com.smartindustry.common.mapper"})
+@ComponentScan({"com.smartindustry.common.security", "com.smartindustry.inventory"})
 @SpringBootApplication
 public class InventoryApplication {
     public static void main(String[] args) {
