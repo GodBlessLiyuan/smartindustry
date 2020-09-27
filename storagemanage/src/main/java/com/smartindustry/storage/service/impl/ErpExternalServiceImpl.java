3bba50f16603210a607e744cd5169ba8a81df77c
xiahui <hui.xia@rpahelper.com>
2020-09-27 15:26:18 +0800

    修改PO订单。

--- storagemanage/src/main/java/com/smartindustry/storage/service/impl/ErpExternalServiceImpl.java
+++ storagemanage/src/main/java/com/smartindustry/storage/service/impl/ErpExternalServiceImpl.java
@@ -1,6 +1,7 @@
 package com.smartindustry.storage.service.impl;
 
 import com.smartindustry.common.mapper.si.MaterialMapper;
+import com.smartindustry.common.pojo.si.MaterialPO;
 import com.smartindustry.common.util.DateUtil;
 import com.smartindustry.common.vo.PageInfoVO;
 import com.smartindustry.common.vo.ResultVO;
@@ -42,33 +43,36 @@ public class ErpExternalServiceImpl implements IErpExternalService {
         head.setOno("PO2020020220200202");
         head.setOdate(new Date());
         head.setSupplier("东南院供应商");
-        head.setBuyer("轩辕先生");
+        head.setBuyer("毛慧芳");
         head.setPdate(DateUtil.date2Str(new Date(), DateUtil.Y_M_D));
+
         List<ErpOrderVO.ErpOrderBodyVO> body = vo.getBody();
-        ErpOrderVO.ErpOrderBodyVO bodyVO = new ErpOrderVO.ErpOrderBodyVO();
-        bodyVO.setMid(1L);
-        bodyVO.setMno("5101000496");
-        bodyVO.setMtype((byte) 1);
-        bodyVO.setMname("原料物料001");
-        bodyVO.setMmodel("SH0001");
-        bodyVO.setMdesc("测试物料，原料类型");
-        bodyVO.setOtotal(1000);
-        body.add(bodyVO);
-        ErpOrderVO.ErpOrderBodyVO bodyVO2 = new ErpOrderVO.ErpOrderBodyVO();
-        bodyVO2.setMid(2L);
-        bodyVO2.setMno("5101000497");
-        bodyVO2.setMtype((byte) 2);
-        bodyVO2.setMname("半成品物料002");
-        bodyVO2.setMmodel("SH0002");
-        bodyVO2.setMdesc("测试物料2，半成品类型");
-        bodyVO2.setOtotal(2000);
-        body.add(bodyVO2);
+        MaterialPO materialPO = materialMapper.selectByPrimaryKey(1L);
+        if (null != materialPO) {
+            body.add(createBVO(materialPO));
+        }
+        MaterialPO materialPO2 = materialMapper.selectByPrimaryKey(2L);
+        if (null != materialPO2) {
+            body.add(createBVO(materialPO2));
+        }
 
         new Thread(() -> {
             // 更新物料信息
         }).start();
 
         return ResultVO.ok().setData(vo);
+    }
+
+    private ErpOrderVO.ErpOrderBodyVO createBVO(MaterialPO po) {
+        ErpOrderVO.ErpOrderBodyVO vo = new ErpOrderVO.ErpOrderBodyVO();
+        vo.setMid(po.getMaterialId());
+        vo.setMno(po.getMaterialNo());
+        vo.setMtype(po.getMaterialType());
+        vo.setMname(po.getMaterialName());
+        vo.setMmodel(po.getMaterialModel());
+        vo.setMdesc(po.getMaterialDesc());
+        vo.setOtotal(1000);
+        return vo;
     }
 
     @Override
@@ -79,7 +83,7 @@ public class ErpExternalServiceImpl implements IErpExternalService {
         head1.setOno("PO2020070100001");
         head1.setOdate(new Date());
         head1.setSupplier("东南院供应商1");
-        head1.setBuyer("轩辕先生1");
+        head1.setBuyer("毛慧芳1");
         head1.setPdate(DateUtil.date2Str(new Date(), DateUtil.Y_M_D));
         vos.add(head1);
 
@@ -87,7 +91,7 @@ public class ErpExternalServiceImpl implements IErpExternalService {
         head2.setOno("PO2020070100002");
         head2.setOdate(new Date());
         head2.setSupplier("东南院供应商2");
-        head2.setBuyer("轩辕先生2");
+        head2.setBuyer("毛慧芳2");
         head2.setPdate(DateUtil.date2Str(new Date(), DateUtil.Y_M_D));
         vos.add(head2);
 
@@ -95,7 +99,7 @@ public class ErpExternalServiceImpl implements IErpExternalService {
         head3.setOno("PO2020070100003");
         head3.setOdate(new Date());
         head3.setSupplier("东南院供应商3");
-        head3.setBuyer("轩辕先生3");
+        head3.setBuyer("毛慧芳3");
         head3.setPdate(DateUtil.date2Str(new Date(), DateUtil.Y_M_D));
         vos.add(head3);
 
@@ -103,7 +107,7 @@ public class ErpExternalServiceImpl implements IErpExternalService {
         head4.setOno("PO2020070100004");
         head4.setOdate(new Date());
         head4.setSupplier("东南院供应商4");
-        head4.setBuyer("轩辕先生4");
+        head4.setBuyer("毛慧芳4");
         head4.setPdate(DateUtil.date2Str(new Date(), DateUtil.Y_M_D));
         vos.add(head4);
 
