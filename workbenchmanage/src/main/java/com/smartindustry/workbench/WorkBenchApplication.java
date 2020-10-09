4afcd3dcc7f11db3df8b9cd94520a14ed7b34ce2
xiahui <hui.xia@rpahelper.com>
2020-10-09 10:40:58 +0800

    修改文件配置。

--- workbenchmanage/src/main/java/com/smartindustry/workbench/WorkBenchApplication.java
+++ workbenchmanage/src/main/java/com/smartindustry/workbench/WorkBenchApplication.java
@@ -1,8 +1,10 @@
 package com.smartindustry.workbench;
 
+import com.smartindustry.common.config.FilePathConfig;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
@@ -14,10 +16,11 @@ import org.springframework.context.annotation.ComponentScan;
  */
 
 @MapperScan("com.smartindustry.common.mapper")
-@ComponentScan({"com.smartindustry.common.security","com.smartindustry.workbench"})
+@ComponentScan({"com.smartindustry.common.security", "com.smartindustry.workbench"})
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@EnableConfigurationProperties({FilePathConfig.class})
 public class WorkBenchApplication {
 
     public static void main(String[] args) {
