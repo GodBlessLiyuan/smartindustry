d099a13ebbf94c5c21f9e9fdc73da1a4a43163b8
chaojie.jiang <chaojie.jiang@vbooster.cn>
2020-11-02 15:30:45 +0800

    erm修改2

--- common-lib/src/main/java/com/smartindustry/common/mapper/om/OutboundForkliftMapper.java
+++ common-lib/src/main/java/com/smartindustry/common/mapper/om/OutboundForkliftMapper.java
@@ -1,5 +1,6 @@
 package com.smartindustry.common.mapper.om;
 
+import com.smartindustry.common.mapper.BaseMapper;
 import com.smartindustry.common.pojo.om.OutboundForkliftPO;
 import org.apache.ibatis.annotations.Mapper;
 
@@ -7,6 +8,19 @@ import org.apache.ibatis.annotations.Mapper;
  * OutboundForkliftMapper继承基类
  */
 @Mapper
-@Mapper
 public interface OutboundForkliftMapper extends BaseMapper<OutboundForkliftPO, Long> {
+    /**
+     * 根据 叉车ID 删除
+     *
+     * @param forkliftId
+     */
+    void deleteByFid(Long forkliftId);
+
+    /**
+     * 根据 叉车ID 查询
+     *
+     * @param forkliftId
+     * @return
+     */
+    OutboundForkliftPO queryByFid(Long forkliftId);
 }
--- common-lib/src/main/java/com/smartindustry/common/mapper/sm/StorageForkliftMapper.java
+++ common-lib/src/main/java/com/smartindustry/common/mapper/sm/StorageForkliftMapper.java
@@ -1,12 +1,12 @@
 package com.smartindustry.common.mapper.sm;
 
+import com.smartindustry.common.mapper.BaseMapper;
 import com.smartindustry.common.pojo.sm.StorageForkliftPO;
 import org.apache.ibatis.annotations.Mapper;
 
 /**
  * StorageForkliftMapper继承基类
  */
-@Mapper
 @Mapper
 public interface StorageForkliftMapper extends BaseMapper<StorageForkliftPO, Long> {
 }
