1292f24ca6ecf8033e1f34aec22943654ff18f89
chaojie.jiang <chaojie.jiang@vbooster.cn>
2020-11-02 10:53:43 +0800

    erm修改

--- common-lib/src/main/java/com/smartindustry/common/pojo/sm/StorageBodyPO.java
+++ common-lib/src/main/java/com/smartindustry/common/pojo/sm/StorageBodyPO.java
@@ -11,26 +11,53 @@ import lombok.Data;
  */
 @Data
 public class StorageBodyPO implements Serializable {
+    /**
+     * 入库单表体ID
+     */
     private Long storageBodyId;
 
+    /**
+     * 入库单表头ID
+     */
     private Long storageHeadId;
 
+    /**
+     * 物料ID
+     */
     private Long materialId;
 
+    /**
+     * 库位ID
+     */
     private Long locationId;
 
+    /**
+     * 车牌信息
+     */
     private String carBrand;
 
+    /**
+     * 期望入库数量
+     */
     private BigDecimal expectNum;
 
+    /**
+     * 已经入库数量
+     */
     private BigDecimal storageNum;
 
+    /**
+     * 接受日期
+     */
     private Date acceptTime;
 
+    /**
+     * 创建时间
+     */
     private Date createTime;
 
     /**
-     * 1：未删除
+     * 是否删除 : 1：未删除
 2：已删除
      */
     private Byte dr;
