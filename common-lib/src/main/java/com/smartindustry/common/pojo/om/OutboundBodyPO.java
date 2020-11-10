54382c1d7cc60c473ca40505d75e7400e8690514
hui.feng <hui.feng@vbooster.cn>
2020-11-10 19:17:32 +0800

    出库表数据同步

--- common-lib/src/main/java/com/smartindustry/common/pojo/om/OutboundBodyPO.java
+++ common-lib/src/main/java/com/smartindustry/common/pojo/om/OutboundBodyPO.java
@@ -3,8 +3,6 @@ package com.smartindustry.common.pojo.om;
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.Date;
-
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 /**
@@ -13,25 +11,61 @@ import lombok.Data;
  */
 @Data
 public class OutboundBodyPO implements Serializable {
+    /**
+     * 出库单表体id
+     */
     private Long outboundBodyId;
 
+    /**
+     * 出库单表头ID
+     */
     private Long outboundHeadId;
 
+    /**
+     * 物料ID
+     */
     private Long materialId;
 
+    /**
+     * 期望出库数量
+     */
     private BigDecimal expectNum;
 
+    /**
+     * 已出库数量
+     */
     private BigDecimal outboundNum;
 
+    /**
+     * 创建时间
+     */
     private Date createTime;
 
+    /**
+     * 出库时间
+     */
     private Date outboundTime;
 
     /**
-     * 1 未删除
+     * 是否删除 : 1 未删除
 2 已删除
      */
     private Byte dr;
+
+    /**
+     * 单价
+     */
+    private BigDecimal unitPrice;
+
+    /**
+     * 金额
+     */
+    private BigDecimal totalPrice;
+
+    /**
+     * 备注
+     */
+    private String remark;
 
     private static final long serialVersionUID = 1L;
 }
--- datasynchronize/src/main/java/com/smartindustry/datasynchronize/service/impl/PurchaseStorageServiceImpl.java
+++ datasynchronize/src/main/java/com/smartindustry/datasynchronize/service/impl/PurchaseStorageServiceImpl.java
@@ -1,15 +1,13 @@
 package com.smartindustry.datasynchronize.service.impl;
 
-import com.smartindustry.common.bo.ds.PurchaseErpBO;
+import com.smartindustry.common.bo.sm.StorageHeadBO;
 import com.smartindustry.common.mapper.si.MaterialMapper;
 import com.smartindustry.common.mapper.si.SupplierMapper;
 import com.smartindustry.common.mapper.sm.StorageBodyMapper;
 import com.smartindustry.common.mapper.sm.StorageHeadMapper;
 import com.smartindustry.common.pojo.ds.sqlserver.PurchaseDetailErpPO;
-import com.smartindustry.common.pojo.si.MaterialPO;
-import com.smartindustry.common.pojo.si.SupplierPO;
+import com.smartindustry.common.pojo.ds.sqlserver.PurchaseErpPO;
 import com.smartindustry.common.pojo.sm.StorageBodyPO;
-import com.smartindustry.common.pojo.sm.StorageHeadPO;
 import com.smartindustry.common.sqlserver.PurchaseErpMapper;
 import com.smartindustry.common.vo.ResultVO;
 import com.smartindustry.datasynchronize.constant.SyncConstant;
@@ -19,7 +17,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.math.BigDecimal;
 import java.util.*;
 
 /**
@@ -54,75 +51,22 @@ public class PurchaseStorageServiceImpl implements IPurchaseStorageService {
     @Override
     @Transactional(rollbackFor = Exception.class)
     public ResultVO sync() {
-        //从erp中获取数据
-        List<PurchaseErpBO> bos = purchaseErpMapper.queryAll();
-        List<StorageHeadPO> hpos = new ArrayList<>(bos.size());
-        Map<String, List<StorageBodyPO>> map = new HashMap<>(hpos.size());
-        for (PurchaseErpBO bo: bos) {
-            StorageHeadPO po = new StorageHeadPO();
-            po.setStorageNo(bo.getPurchaseNo());
-            if (storageHeadMapper.queryByStorageNo(po.getStorageNo()) != null) {
-                continue;
-            }
-            po.setSourceType((byte)1);
-            po.setSourceNo(bo.getOrderNo());
-            po.setStorageTime(bo.getAcceptDate());
-            if (bo.getNum() != null) {
-                po.setExpectNum(BigDecimal.valueOf(bo.getNum()));
-            } else {
-                po.setExpectNum(BigDecimal.valueOf(0));
-            }
-            po.setExtra(bo.getRemark());
-            po.setStatus((byte)3);
-            po.setCreateTime(Calendar.getInstance().getTime());
-            po.setDr((byte)1);
-            po.setPayMethod(bo.getPayMethod().equals(SyncConstant.PAY_METHOD_ERP_BILL) ?SyncConstant.PAY_METHOD_BILL:SyncConstant.PAY_METHOD_DEBT);
-            SupplierPO supplier = supplierMapper.queryBySno(bo.getSupplierNo());
-            if (supplier == null) {
-                continue;
-            }
-            hpos.add(po);
-            if (bo.getPdpos() != null && !bo.getPdpos().isEmpty()) {
-                map.put(po.getStorageNo(), convert(bo.getPdpos(), po.getStorageTime(), supplier.getSupplierId()));
-            }
-        }
-        if (!hpos.isEmpty()) {
-            storageHeadMapper.batchInsert(hpos);
-            List<StorageBodyPO> pos = new ArrayList<>(bos.size());
-            for (StorageHeadPO po: hpos) {
-                List<StorageBodyPO> bpos = map.get(po.getStorageNo());
-                for (StorageBodyPO bpo: bpos) {
-                    bpo.setStorageHeadId(po.getStorageHeadId());
-                    pos.add(bpo);
-                }
-            }
-            if (!pos.isEmpty()) {
-                storageBodyMapper.batchInsert(pos);
-            }
-        }
+        Map<String, Object>reqData = new HashMap<>();
+         List<StorageHeadBO> bos = storageHeadMapper.pageQuery(reqData);
+         List<PurchaseErpPO> pos = new ArrayList<>(bos.size());
+         Map<String, List<PurchaseDetailErpPO>> map = new HashMap<>(bos.size());
+         for (StorageHeadBO bo : bos) {
+             PurchaseErpPO po = new PurchaseErpPO();
+             po.setPurchaseNo(bo.getStorageNo());
+             po.setRemark(bo.getExtra());
+             po.setPayMethod(SyncConstant.PAY_METHOD_DEBT.equals(bo.getPayMethod())?SyncConstant.PAY_METHOD_ERP_DEBT: SyncConstant.PAY_METHOD_ERP_BILL);
+             po.setPayName(SyncConstant.PAY_METHOD_DEBT.equals(bo.getPayMethod())?SyncConstant.PAY_NAME_ERP_DEBT: SyncConstant.PAY_NAME_ERP_BILL);
+             po.setAcceptDate(bo.getStorageTime());
+         }
         return ResultVO.ok();
     }
 
     private List<StorageBodyPO> convert(List<PurchaseDetailErpPO> sdpos, Date acceptDate, Long supplierId) {
-        List<StorageBodyPO> pos = new ArrayList<>(sdpos.size());
-        for (PurchaseDetailErpPO ep: sdpos) {
-            StorageBodyPO po = new StorageBodyPO();
-            po.setSupplierId(supplierId);
-            MaterialPO material = materialMapper.queryByMaterialNo(ep.getMaterialNo(), ep.getMaterialName());
-            if (material == null){
-                continue;
-            }
-            po.setMaterialId(material.getMaterialId());
-            po.setExpectNum(BigDecimal.valueOf(ep.getNum()));
-            po.setCreateTime(Calendar.getInstance().getTime());
-            po.setDr((byte)1);
-            po.setAcceptTime(acceptDate);
-            po.setUnitPrice(BigDecimal.valueOf(ep.getUnitPrice()));
-            po.setSumPrice(BigDecimal.valueOf(ep.getSum()));
-            po.setUnitPriceNotax(BigDecimal.valueOf(ep.getUnitPriceNoTax()));
-            po.setSumPriceNotax(BigDecimal.valueOf(ep.getSumNoTax()));
-            pos.add(po);
-        }
-        return pos;
+        return null;
     }
 }
