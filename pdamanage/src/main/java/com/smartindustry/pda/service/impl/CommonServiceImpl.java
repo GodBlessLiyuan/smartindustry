d3c33ad84498a90685baa8ef647ada5859007de7
xiahui <hui.xia@rpahelper.com>
2020-11-16 16:13:54 +0800

    修改提示信息。

--- pdamanage/src/main/java/com/smartindustry/pda/service/impl/CommonServiceImpl.java
+++ pdamanage/src/main/java/com/smartindustry/pda/service/impl/CommonServiceImpl.java
@@ -299,6 +299,7 @@ public class CommonServiceImpl implements ICommonService {
                 imeis.add(ofBO.getImeiNo());
             }
 
+            WebSocketVO socketVO = new WebSocketVO();
             if (headPO.getExpectNum().equals(headPO.getOutboundNum())) {
                 // 出库完成
                 headPO.setStatus((byte) 1);
@@ -307,10 +308,14 @@ public class CommonServiceImpl implements ICommonService {
                 outboundForkliftMapper.deleteByOhid(headPO.getOutboundHeadId());
 
                 // websocket
-                WebSocketServer.sendMsg(imeis, WebSocketVO.createTitleVO("业务单号：" + headPO.getSourceNo() + "（销售出库），已完成作业任务，任务关闭", CommonConstant.TYPE_TITLE_INTO));
-            } else {
-                WebSocketServer.sendMsg(imeis, WebSocketVO.createShowVO(headPO.getOutboundHeadId(), CommonConstant.FLAG_OUTBOUND));
+                WebSocketVO.TitleVO titleVO = new WebSocketVO.TitleVO();
+                titleVO.setMsg("业务单号：" + headPO.getSourceNo() + "（销售出库），已完成作业任务，任务关闭");
+                titleVO.setType(CommonConstant.TYPE_TITLE_INTO);
+                socketVO.setTitle(titleVO);
             }
+
+            socketVO.setShow(new WebSocketVO.ShowVO(headPO.getOutboundHeadId(), CommonConstant.FLAG_OUTBOUND));
+            WebSocketServer.sendMsg(imeis, socketVO);
 
             outboundHeadMapper.updateByPrimaryKey(headPO);
 
